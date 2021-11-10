a69484cd7646b7b7ed70975d954b4c08bb9cee61
ramabadash <89576646+ramabadash@users.noreply.github.com>
2021-11-10 18:09:54 +0200

    # Front  - on load will generate cities names for a designated list + Base on API request
    1) "getAllCities()" - API request
    
    2) "enerateCitiesToDom()" - append city names to DOM

--- front-end/app/main.js
+++ front-end/app/main.js
@@ -1,4 +1,42 @@
 import './styles/index.css';
-import { hello } from './js/other';
 
-hello();
+/* ---------- VARIIABLES DECLARATION ----------*/
+const BASEURL = '';
+
+const citiesSelectBar = document.getElementById('cities');
+
+/* ---------- EVENT LISTENERS ----------*/
+window.addEventListener('load', generateCitiesToDom);
+/* ---------- NETWORK ----------*/
+// API request for all cities names
+async function getAllCities() {
+  try {
+    const response = await axios.get(`${BASEURL}/cities`);
+    const citiesArray = response.data;
+    return citiesArray;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+/* ---------- CITY LIST ----------*/
+async function generateCitiesToDom() {
+  try {
+    const citiesArray = await getAllCities();
+    for (const name of citiesArray) {
+      const cityElem = createElement('option', name, 'city-name');
+      citiesSelectBar.appendChild(cityElem);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+/* ---------- DOM RELATED ----------*/
+// General create element function
+function createElement(tagName, textContent, className) {
+  const newElem = document.createElement(tagName);
+  newElem.textContent = textContent;
+  newElem.classList.add(className);
+  return newElem;
+}
