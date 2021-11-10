96d582b5f263df931156c9f83b4b58f4149f5745
ramabadash <89576646+ramabadash@users.noreply.github.com>
2021-11-10 18:51:28 +0200

    # Front - Add functionality to display a list of agent names when choosing their city ON CITIES LIST CHANGE
    1) "showAgentsList" - Displays the names in the DOM
    
    2) "getNamesByCity" - Sends an API request and receives an array of agents from a particular city.

--- front-end/app/main.js
+++ front-end/app/main.js
@@ -5,8 +5,52 @@ const BASEURL = '';
 
 const citiesSelectBar = document.getElementById('cities');
 
-/* ---------- EVENT LISTENERS ----------*/
-window.addEventListener('load', generateCitiesToDom);
+/* ---------- DOM RELATED ----------*/
+// General create element function
+function createElement(tagName, textContent, className) {
+  const newElem = document.createElement(tagName);
+  newElem.textContent = textContent;
+  newElem.classList.add(className);
+  return newElem;
+}
+
+/* ---------- CITY LIST ----------*/
+async function generateCitiesToDom() {
+  try {
+    const citiesArray = await getAllCities();
+    for (const name of citiesArray) {
+      const cityElem = createElement('option', name, 'city-name');
+      cityElem.value = name;
+      citiesSelectBar.appendChild(cityElem);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+/* ---------- AGENTS LIST ----------*/
+async function showAgentsList(event) {
+  try {
+    removeAgentsList();
+    const agentsArray = await getNamesByCity(event.target.value);
+    const agentsList = createElement('select', '', 'agents-select');
+    for (const agent of agentsArray) {
+      const agentElem = createElement('option', agent.full_name, 'agent-name');
+      agentElem.value = agent.full_name;
+      agentsList.appendChild(agentElem);
+    }
+    const mainElem = document.getElementsByTagName('main')[0];
+    mainElem.appendChild(agentsList);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function removeAgentsList() {
+  const agentsList = document.querySelector('.agents-select');
+  if (agentsList) agentsList.remove();
+}
+
 /* ---------- NETWORK ----------*/
 // API request for all cities names
 async function getAllCities() {
@@ -19,24 +63,16 @@ async function getAllCities() {
   }
 }
 
-/* ---------- CITY LIST ----------*/
-async function generateCitiesToDom() {
+// Get all agents from city name
+async function getNamesByCity(city) {
   try {
-    const citiesArray = await getAllCities();
-    for (const name of citiesArray) {
-      const cityElem = createElement('option', name, 'city-name');
-      citiesSelectBar.appendChild(cityElem);
-    }
+    const response = await axios.get(`${BASEURL}/agents/?city=${city}`);
+    return response.data;
   } catch (error) {
     console.log(error);
   }
 }
 
-/* ---------- DOM RELATED ----------*/
-// General create element function
-function createElement(tagName, textContent, className) {
-  const newElem = document.createElement(tagName);
-  newElem.textContent = textContent;
-  newElem.classList.add(className);
-  return newElem;
-}
+/* ---------- EVENT LISTENERS ----------*/
+window.addEventListener('load', generateCitiesToDom);
+citiesSelectBar.addEventListener('change', showAgentsList);
