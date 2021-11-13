a580d3a9ee83ff7321b1e318e43a13c6080d308e
ramabadash <89576646+ramabadash@users.noreply.github.com>
2021-11-13 19:08:16 +0200

    # Code style changes

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,12 +1,12 @@
 module.exports = {
-    env: {
-      es2021: true,
-      browser: true,
-    },
-    extends: ['airbnb-base'],
-    parserOptions: {
-      ecmaVersion: 12,
-      sourceType: 'module',
-    },
-    rules: {},
-  };
+  env: {
+    es2021: true,
+    browser: true,
+  },
+  extends: ['airbnb-base'],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {},
+};
